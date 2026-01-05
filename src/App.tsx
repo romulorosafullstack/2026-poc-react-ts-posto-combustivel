d270a10c70851e3e214025ee583e7f66699765db
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2026-01-05 15:22:43 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header/header";
+
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,11 @@
+import './header.scss'
+
+export default function Header() {
+  return (
+    <header className="header--wrapper">
+      <div className="header--content">
+        <h1 className="header--title">Header</h1>
+      </div>
+    </header>
+  );
+}
